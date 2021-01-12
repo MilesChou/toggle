67bcd1195df25875f2aa73a9976ea91cc2e10240
MilesChou <jangconan@gmail.com>
2021-01-12 23:49:57 +0800

    Add service provider to add directive (#1)

---
+++ tests/Toggle/ServiceProvider/BladeServiceProviderTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Toggle;
+
+use Illuminate\Events\EventServiceProvider;
+use Illuminate\Filesystem\FilesystemServiceProvider;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Testing\TestCase;
+use Illuminate\Support\Fluent;
+use Illuminate\View\Compilers\BladeCompiler;
+use Illuminate\View\Engines\EngineResolver;
+use Illuminate\View\Factory;
+use Illuminate\View\ViewServiceProvider;
+use MilesChou\Toggle\ServiceProvider\BladeServiceProvider;
+use MilesChou\Toggle\Toggle;
+
+class BladeServiceProviderTest extends TestCase
+{
+    public function createApplication()
+    {
+        $app = new Application();
+        $app->register(EventServiceProvider::class);
+        $app->register(FilesystemServiceProvider::class);
+        $app->register(ViewServiceProvider::class);
+
+        $app->register(BladeServiceProvider::class);
+
+        $app->singleton(Toggle::class);
+
+        $app->instance('config', new Fluent([
+            'view.paths' => ['/tmp'],
+            'view.compiled' => '/tmp',
+        ]));
+
+        return $app;
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReturnFalseWhenUsingIsActiveWithToggleIsNotSet()
+    {
+        $this->app->boot();
+
+        /** @var EngineResolver $view */
+        $view = $this->app->make('view.engine.resolver');
+
+        /** @var BladeCompiler $blade */
+        $blade = $view->resolve('blade')->getCompiler();
+
+        $actual = $blade->compileString("@isActive('feature')");
+
+        $this->assertSame('<?php if (false): ?>', $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReturnTrueWhenUsingIsInactiveWithToggleIsNotSet()
+    {
+        $this->app->boot();
+
+        /** @var EngineResolver $view */
+        $view = $this->app->make('view.engine.resolver');
+
+        /** @var BladeCompiler $blade */
+        $blade = $view->resolve('blade')->getCompiler();
+
+        $actual = $blade->compileString("@isInactive('feature')");
+
+        $this->assertSame('<?php if (true): ?>', $actual);
+    }
+}
